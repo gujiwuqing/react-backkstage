2db7662abc30efdf62223262d9361577deef7020
fwh <15279279713@163.com>
2021-03-30 22:09:55 +0800

    修改页签栏样式

--- src/layouts/BaseTab/index.tsx
+++ src/layouts/BaseTab/index.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import { useModel, history } from 'umi';
-import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 import './index.less';
 import { useTranslation } from 'react-i18next';
+import { Menu, Dropdown } from 'antd';
 interface tabItem {
   title: string;
   path: string;
   name: string;
 }
 export default function index() {
+  const pathname = history.location.pathname;
   const { t, i18n } = useTranslation();
   const { tabList, changedTabList } = useModel('tab-list', (model) => ({
     tabList: model.tabList,
     changedTabList: model.changedTabList,
   }));
-  function handleClick(e: any, data: any) {
-    console.log(e);
-    const name = data.target.outerText;
-    const { choose } = data;
-    if (choose == '') {
-    } else if (choose == 'other') {
-    }
-  }
+  const renderMenus = (item: any) => (
+    <Menu>
+      <Menu.Item key="refresh">
+        <div>刷新</div>
+      </Menu.Item>
+      {item.path !== '/' && item.path == pathname && (
+        <Menu.Item key="close-current">
+          <div>关闭当前页签</div>
+        </Menu.Item>
+      )}
+      <Menu.Item key="close-other">
+        <div>关闭其它页签</div>
+      </Menu.Item>
+      <Menu.Item key="close-all">
+        <div>关闭全部</div>
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <>
       <div className="tab-list">
         {tabList.map((item: tabItem) => {
           return (
-            <ContextMenuTrigger id="same_unique_identifier">
+            <Dropdown
+              overlay={() => renderMenus(item)}
+              trigger={['contextMenu']}
+            >
               <div
                 className={
                   item.path === history.location.pathname
@@ -41,24 +55,11 @@ export default function index() {
               >
                 {' '}
                 {t(item.name)}
-                {/* {item.title} */}
               </div>
-            </ContextMenuTrigger>
+            </Dropdown>
           );
         })}
       </div>
-      <ContextMenu id="same_unique_identifier">
-        <MenuItem data={{ choose: 'bar' }} onClick={handleClick}>
-          刷新
-        </MenuItem>
-        <MenuItem data={{ choose: 'other' }} onClick={handleClick}>
-          关闭其他
-        </MenuItem>
-        {/* <MenuItem divider /> */}
-        <MenuItem data={{ choose: 'all' }} onClick={handleClick}>
-          关闭所有
-        </MenuItem>
-      </ContextMenu>
     </>
   );
 }
