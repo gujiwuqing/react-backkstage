4add646c8307c06c4d92a468b6a13742340fe293
fwh <15279279713@163.com>
2021-06-08 11:33:58 +0800

    fix bug

--- .umirc.ts
+++ .umirc.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'umi';
 import routes from './config/routes';
 
 export default defineConfig({
-  base: '/react-backstage',
+  base: '/react-backstage/',
   nodeModulesTransform: {
     type: 'none',
   },
+  publicPath:
+    process.env.NODE_ENV === 'development'
+      ? './'
+      : 'https://gujiwuqing.github.io/react-backstage/',
+
+  /**
+   路由要改成 hash路由，不然打开页面也是空白的。
+   在发布到gitee之前，可以先用 ./ 打包，在本地试试，
+   看看打开index.html有没有效果
+   */
+  history: { type: 'hash' },
   routes,
   devServer: {
     port: 8009,
